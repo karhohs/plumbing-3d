580a6014a9b861164f316d45a960e2708be32c54
Kyle <gandalfthegradient@gmail.com>
2026-06-17 21:44:18 -0400

    remove reference to legacy parts.py file in the __init__.py file.

--- src/plumbing_3d/__init__.py
+++ src/plumbing_3d/__init__.py
@@ -1,4 +1,4 @@
-"""Starter package for plumbing part helpers."""
+"""A package for plumbing part helpers."""
 
 from .elbow_extension import (
 	ElbowExtensionDimensions,
@@ -6,14 +6,11 @@ from .elbow_extension import (
 	export_elbow_extension_stl,
 	starter_elbow_extension_dimensions,
 )
-from .parts import PipeDimensions, starter_pipe_dimensions
 
 __all__ = [
 	"ElbowExtensionDimensions",
-	"PipeDimensions",
 	"build_elbow_extension",
 	"export_elbow_extension_stl",
 	"starter_elbow_extension_dimensions",
-	"starter_pipe_dimensions",
 ]
 __version__ = "0.1.0"
